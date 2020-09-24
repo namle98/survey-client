9ae200f4800e085497dbc6a016e7a38dced59059
Đại <thantrongdai94@gmail.com>
2020-09-24 14:15:46 +0700

    update url

--- src/app/config/url.js
+++ src/app/config/url.js
@@ -1,5 +1,2 @@
-// export const URL_API = 'https://survey.sky-demo.net/api/';
-export const URL_API = 'http://localhost:8000/api/';
-// export const URL_API = 'http://127.0.0.1:3098/api/';
+export const URL_API = 'http://127.0.0.1:3098/api/';
 export const URL_COPY = 'https://survey.sky-demo.net/formanswer?';
- //export const URL_API = 'http://127.0.0.1:3229/api/';
